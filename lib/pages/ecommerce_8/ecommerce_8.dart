cba0fe86e57192d828ac453be35e70635b5d4c4d
Huy Phong <doivjpxx@gmail.com>
2021-01-21 22:44:28 +0700

    completed ecommerce 8

--- lib/pages/ecommerce_8/ecommerce_8.dart
+++ lib/pages/ecommerce_8/ecommerce_8.dart
@@ -57,7 +57,76 @@ class MyEcommerce8 extends StatelessWidget {
     if (index == 1) {
       return _buildCategoriesGrid();
     }
-    return Container();
+    if (index == 2) {
+      return _buildFlashSale();
+    }
+    if (index == 3) {
+      return _buildCategories();
+    }
+    if (index == 4)
+      return Center(
+          child: Text(
+        'Just for You',
+        style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20.0),
+      ));
+    return Container(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              children: <Widget>[
+                PNetworkImage(images[index % images.length]),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Top Quality fashion item',
+                  softWrap: true,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Rs.1,254',
+                  style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            width: 20.0,
+            height: 10.0,
+          ),
+          Expanded(
+            child: Column(
+              children: <Widget>[
+                PNetworkImage(images[(index - 1) % images.length]),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Top Quality fashion item',
+                  softWrap: true,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Rs.1,254',
+                  style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   Widget _buildSlider() {
@@ -123,5 +192,187 @@ class MyEcommerce8 extends StatelessWidget {
                 ),
               );
             }));
+  }
+
+  Widget _buildFlashSale() {
+    return Container(
+      height: 200.0,
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Text(
+                    'Flash Sales',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(width: 10.0),
+                  TimerBox(
+                    time: '02',
+                  ),
+                  SizedBox(width: 5.0),
+                  TimerBox(
+                    time: '20',
+                  ),
+                  SizedBox(width: 5.0),
+                  TimerBox(
+                    time: '50',
+                  ),
+                ],
+              ),
+              Text(
+                'SHOP NOW >> ',
+                style: TextStyle(color: Colors.red),
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              _buildProductBox(0),
+              _buildProductBox(1),
+              _buildProductBox(2),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCategories() {
+    return Container(
+      height: 180.0,
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              children: <Widget>[
+                _buildPopularItem(),
+                SizedBox(
+                  width: 20.0,
+                ),
+                _buildPopularItem()
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              children: <Widget>[
+                _buildPopularItem(),
+                SizedBox(
+                  width: 20.0,
+                ),
+                _buildPopularItem()
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+extension MoreMyEcommerce8 on MyEcommerce8 {
+  Widget _buildProductBox(index) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 80,
+              // color: Colors.blue,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(flashSaleImages[index]),
+                      fit: BoxFit.cover)),
+            ),
+            SizedBox(
+              height: 5.0,
+            ),
+            Stack(
+              children: <Widget>[
+                ClipPath(
+                  clipper: ShapeBorderClipper(
+                      shape: StadiumBorder(
+                          side: BorderSide(
+                              width: 1,
+                              style: BorderStyle.solid,
+                              color: Colors.red))),
+                  child: Container(
+                    height: 20,
+                    color: Colors.red.shade200,
+                  ),
+                ),
+                ClipPath(
+                  clipper: ShapeBorderClipper(
+                      shape: StadiumBorder(
+                          side: BorderSide(
+                              width: 1,
+                              style: BorderStyle.solid,
+                              color: Colors.red))),
+                  child: Container(
+                    padding: EdgeInsets.only(left: 10.0),
+                    height: 20,
+                    width: 70,
+                    color: Colors.red,
+                    child: Text(
+                      '12 Sold',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPopularItem() {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.grey.shade200,
+            border: Border(
+                left: BorderSide(
+                    color: Colors.red.shade200,
+                    style: BorderStyle.solid,
+                    width: 5))),
+        child: ListTile(
+          onTap: () {},
+          title: Text("Vehicles"),
+          subtitle: Text('120 people want this'),
+          trailing: Container(
+              width: 50,
+              child: PNetworkImage(
+                assets.backgroundImages[1],
+                fit: BoxFit.cover,
+              )),
+        ),
+      ),
+    );
+  }
+}
+
+class TimerBox extends StatelessWidget {
+  final String time;
+
+  const TimerBox({Key key, @required this.time}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.black,
+        padding: EdgeInsets.symmetric(vertical: 2.0, horizontal: 5.0),
+        child: Text(
+          time,
+          style: TextStyle(color: Colors.white),
+        ));
   }
 }
